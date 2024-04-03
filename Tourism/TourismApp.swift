1b7de9ba1b9ca2d6615af904a4d5e8072a55c21c
Luis Bustamante <luisebt995@gmail.com>
2024-04-03 19:28:30 +0200

    Initial Commit

---
+++ Tourism/TourismApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourismApp.swift
+//  Tourism
+//
+//  Created by Luis on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TourismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
